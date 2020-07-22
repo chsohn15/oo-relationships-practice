a670a928adbae8f9ebb46856ee9b0353131fb1d6
chsohn15 <chsohn15@gmail.com>
2020-07-22 19:23:29 -0400

    Added methods to Actor class

--- app/models/imdb/actor.rb
+++ app/models/imdb/actor.rb
@@ -1,7 +1,30 @@
 class Actor 
 
+    @@all = []
     def initialize(name)
         @name = name
+
+        @@all << self
     end
     
+    def self.all
+        @@all
+    end
+
+    def all_characters
+        #Returns all characters the actor has played
+        Character.all.select do |character|
+            character.actor == self
+        end
+    end
+
+    def num_of_characters
+        self.all_characters.length
+    end
+
+    def self.most_characters
+        Actor.all.max_by do |actor|
+            actor.num_of_characters
+        end
+    end
 end
